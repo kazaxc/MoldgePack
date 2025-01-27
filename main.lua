da2345e6e47513e0618af2ec23e49c484a164388
kazaxc <lgkscast@gmail.com>
2025-01-27 19:45:55 +0000

    Small fix to sounds played

--- main.lua
+++ main.lua
@@ -563,7 +563,9 @@ SMODS.Joker{
 
             return {
                 mult_mod = card.ability.extra.mult,
-                message = localize { type = 'variable', key = 'a_mult', vars = { card.ability.extra.mult } }
+                message = localize { type = 'variable', key = 'a_mult', vars = { card.ability.extra.mult } },
+                sound = 'mldg_glassBreak',
+                card = card
             }
         end
     end
